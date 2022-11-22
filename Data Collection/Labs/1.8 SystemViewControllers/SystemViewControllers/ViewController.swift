bf6b909b7fb0376f5bfec9544a20a0c105453f85
Oleh Dorozhovers <olzer123@gmail.com>
2022-11-22 15:12:44 +0100

    Added new project of Apple Curriculum Data Collection course. Empty project

---
+++ Data Collection/Labs/1.8 SystemViewControllers/SystemViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Oleg Dorozhovets on 22/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
