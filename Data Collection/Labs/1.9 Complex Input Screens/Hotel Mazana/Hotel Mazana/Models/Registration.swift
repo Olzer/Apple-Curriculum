6ab012e108f8949902e0ccd34cd7094e147d6199
Oleh Dorozhovers <olzer123@gmail.com>
2022-12-07 14:22:38 +0100

    Added 'Registration' & 'RoomType' models

---
+++ Data Collection/Labs/1.9 Complex Input Screens/Hotel Mazana/Hotel Mazana/Models/Registration.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct Registration {
+    var firstName: String
+    var lastName: String
+    var emailAdress: String
+    
+    var checkIndate: Int
+    var checkOutDate: Int
+    var numberOfAdult: Int
+    var naumberOfchildren: Int
+    
+    var wifi: Bool
+    var price: Int
+}
---
+++ Data Collection/Labs/1.9 Complex Input Screens/Hotel Mazana/Hotel Mazana/Models/RoomType.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct RoomType {
+    var id: Int
+    var name: String
+    var shortName: String
+    var price: Int
+    
+    static func ==(lhs: RoomType, rhs: RoomType) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
