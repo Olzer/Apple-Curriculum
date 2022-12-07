78ef7b38010c9020d4e56ccb777aba03269d6362
Oleh Dorozhovers <olzer123@gmail.com>
2022-12-07 17:44:27 +0100

    Added new tableView sections and cell's. In new cell, can add numbers of adults and children with stepper. Added labels nad @IBActions

--- Data Collection/Labs/1.9 Complex Input Screens/Hotel Mazana/Hotel Mazana/VC/AddRegistrationTableViewController.swift
+++ Data Collection/Labs/1.9 Complex Input Screens/Hotel Mazana/Hotel Mazana/VC/AddRegistrationTableViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class AddRegistrationTableViewController: UITableViewController {
-
+    
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -11,16 +11,41 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet weak var checkInDatePicker: UIDatePicker!
     @IBOutlet weak var checkOutDatePicker: UIDatePicker!
     
+    @IBOutlet weak var numberOfAdultsLabel: UILabel!
+    @IBOutlet weak var numberOfChildrenLabel: UILabel!
+    @IBOutlet weak var numberOfAdultsStepper: UIStepper!
+    @IBOutlet weak var numberOfChildrenStepper: UIStepper!
+    
+    
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    let checkInDateLabelCellIndexPath = IndexPath(row: 0, section: 1)
+    let checkOutDateLabelCellIndexPath = IndexPath(row: 2, section: 1)
+    
+    var isCheckInDatePickerVisible: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerVisible
+        }
+    }
+    
+    var isCheckOutDatePickerVisible: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerVisible
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-        updateDateViews()
     }
     
     func updateUI() {
         let midNightToday = Calendar.current.startOfDay(for: Date())
         checkInDatePicker.minimumDate = midNightToday
+        checkInDatePicker.maximumDate = .none
         checkOutDatePicker.date = midNightToday
+        updateDateViews()
+        updateNumberOfGuests()
     }
     
     func updateDateViews() {
@@ -29,12 +54,19 @@ class AddRegistrationTableViewController: UITableViewController {
         checkOutDateLabel.text = checkOutDatePicker.date.formatted(date: .abbreviated, time: .omitted)
     }
     
+    func updateNumberOfGuests() {
+        numberOfAdultsLabel.text = "\(Int(numberOfAdultsStepper.value))"
+        numberOfChildrenLabel.text = "\(Int(numberOfChildrenStepper.value))"
+    }
+    
     @IBAction func doneBarButtonTapped(_ sender: UIBarButtonItem) {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
         let email = emailTextField.text ?? ""
         let checkInDate = checkInDateLabel.text ?? ""
         let checkOutDate = checkOutDateLabel.text ?? ""
+        let numberOfAdults = numberOfAdultsLabel.text ?? ""
+        let numberOfChildren = numberOfChildrenLabel.text ?? ""
         
         print("DONE TAPPED")
         print("firstName: \(firstName)")
@@ -42,11 +74,51 @@ class AddRegistrationTableViewController: UITableViewController {
         print("email: \(email)")
         print("checkIn: \(checkInDate)")
         print("checkOut: \(checkOutDate)")
+        print("numberOfAdults: \(numberOfAdults)")
+        print("numberOfChildren: \(numberOfChildren)")
     }
     
     @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
         updateDateViews()
     }
     
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        updateNumberOfGuests()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath where isCheckInDatePickerVisible == false: return 0
+        case checkOutDatePickerCellIndexPath where isCheckOutDatePickerVisible == false: return 0
+        default: return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath: return 190
+        case checkOutDatePickerCellIndexPath: return 190
+        default: return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath == checkInDateLabelCellIndexPath && isCheckOutDatePickerVisible == false {
+            isCheckInDatePickerVisible.toggle()
+        } else if indexPath == checkOutDateLabelCellIndexPath && isCheckInDatePickerVisible == false {
+            isCheckOutDatePickerVisible.toggle()
+        } else if indexPath == checkInDateLabelCellIndexPath || indexPath == checkOutDateLabelCellIndexPath {
+            isCheckInDatePickerVisible.toggle()
+            isCheckOutDatePickerVisible.toggle()
+        } else {
+            return
+        }
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
     
 }
+
